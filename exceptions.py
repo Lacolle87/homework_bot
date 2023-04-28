3e2a9bb2d44a10dcb1caf615814aeaeb9d6635cf
Nikita Kuznetsov <123502774+Lacolle87@users.noreply.github.com>
2023-04-29 01:16:30 +0300

    init

---
+++ exceptions.py
@@ -0,0 +1,22 @@
+class NotForSending(Exception):
+    """Не для пересылки в телеграм."""
+
+
+class ProblemDescriptions(Exception):
+    """Описания проблемы."""
+
+
+class InvalidResponseCode(Exception):
+    """Не верный код ответа."""
+
+
+class ConnectinError(Exception):
+    """Не верный код ответа."""
+
+
+class EmptyResponseFromAPI(NotForSending):
+    """Пустой ответ от API."""
+
+
+class TelegramError(NotForSending):
+    """Ошибка телеграма."""
