3b3e0430b62d83c9249c75b91d0609282c3bf046
Nikita Kuznetsov <123502774+Lacolle87@users.noreply.github.com>
2023-04-30 00:31:14 +0300

    init

--- homework.py
+++ homework.py
@@ -10,12 +10,11 @@ from http import HTTPStatus
 
 load_dotenv()
 
-RETRY_PERIOD = 600
-
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
